81326ff5b0a1da89afd81a76daa43ee272805863
implaymo <136201112+implaymo@users.noreply.github.com>
2024-02-18 21:18:04 +0000

    Manage to see front_page.html when entering website.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import front_page
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('front_page/', front_page, name='front_page'),
+    path("", views.front_page, name="front_page"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 def front_page(request):
     return render(request, 'front_page.html')
